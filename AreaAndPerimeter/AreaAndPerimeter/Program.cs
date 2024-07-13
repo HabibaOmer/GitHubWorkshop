76f1105e2e30c673ab0013030b6133796e70d73c
Habiba Omer <habibaomer675@gmail.com>
2024-07-13 17:27:13 +0300

    Add triangle function

---
+++ AreaAndPerimeter/AreaAndPerimeter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaAndPerimeter
+{
+    class Program
+    {
+        //2
+        public static void Triangle()
+        {
+            Console.WriteLine("The Area Of Triangle: ");
+            Console.WriteLine("Enter the baselength of the triangle: ");
+            double baselength = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the heigth of the triangle: ");
+            double heigth = double.Parse(Console.ReadLine());
+            double area = 0.5 * baselength * heigth;
+            Console.WriteLine($"The Area Of Triangle = {area}.");
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("The perimeter Of Triangle: ");
+            Console.WriteLine("Enter the length of the first side of the triangle: ");
+            double side1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the length of the secound side of the triangle: ");
+            double side2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the length of the third side of the triangle: ");
+            double side3 = double.Parse(Console.ReadLine());
+            double perimeter = side1 + side2 + side3;
+            Console.WriteLine($"The perimeter Of Triangle = {perimeter}.");
+        }
+        static void Main(string[] args)
+        {
+            Triangle();
+            Console.ReadLine();
+        }
+    }
+}
